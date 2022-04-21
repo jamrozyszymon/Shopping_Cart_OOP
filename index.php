f94b47fa9e3fc01ac7ddcae96bf48f21bdf8d1ca
Szymon Jamroży <jamrozyszymon.com>
2022-04-21 17:16:26 +0200

    css

--- index.php
+++ index.php
@@ -26,6 +26,12 @@ $cart->addProduct($p3);
 // Delete a product (product):
 //$cart->deleteProduct($p1);
 
+//Export to .csv
+$cart->exportCsv();
+
+//Export to .txt
+$cart->exportTxt();
+
 // Content of cart:
 ?>
 <!DOCTYPE html>
@@ -35,51 +41,51 @@ $cart->addProduct($p3);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart</title>
+    <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css"/>
 </head>
 <body>
-    <h1>Twój Koszyk</h1>
-    </br>
-    <h2> Zawartość Koszyka</h2>
+    <header>
+        <h1>Twój Koszyk</h1>
+    </header>
+    <main>
+        <h2> Zawartość Koszyka</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nazwa Produktu</th>
+                    <th scope="col">Kod Produktu</th>
+                    <th scope="col">Ilość</th>
+                    <th scope="col">Cena</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($cart as $list) {
+                $product = $list['product'];
+                ?>
+                <tr>
+                    <td> <?php echo $product->getName();?> </td>
+                    <td> <?php echo $product->getCode();?> </td>
+                    <td> <?php echo $list['amount'];?> </td>
+                    <td> <?php echo $product->getPrice(); ?> zł</td>
+                    <td>  
+            <?php
+            }
+            ?>
+            </tbody>
+        </table>
+        <div class="mainfooter">
+            <h3>Podsumowanie</h3>
+            <div>
+                Ilość produktów w koszyku: <?php echo $cart->sumProducts();?>.
+            </div>
+            <div>
+                Wartość produktów: <?php echo $cart->getPriceAll();?> zł.
+            </div>
+        </div>
+    </main>
 </body>
 </html>
-<table>
-    <thead>
-        <tr>
-            <th>Nazwa</th>
-            <th>Kod</th>
-            <th>Ilość</th>
-            <th>Cena</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach($cart as $list) {
-        $product = $list['product'];
-        ?>
-        <tr>
-            <td> <?php echo $product->getName();?></td>
-            <td> <?php echo $product->getCode();?></td>
-            <td> <?php echo $list['amount'];?></td>
-            <td> <?php echo $product->getPrice(); ?> </td>
-            <td>  
-    </tbody>
-    <?php
-    }
-    ?>
-</table>
-</br>
 
 <?php
-
-//Return amount of all product
-echo 'Ilość produktów w koszyku: '. $cart->sumProducts();
-echo '</br>';
-
-//Return total price
-echo 'Wartość produktów: '. $cart->getPriceAll();
-
-//Export to .csv
-$cart->exportCsv();
-
-//Export to .txt
-$cart->exportTxt();
